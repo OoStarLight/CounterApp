17f4a78cc4752f087278378ede16cb160ac571f3
Mehdi LAHAK <mehdi.lahak@oumdin.com>
2023-03-21 11:35:00 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+
+let counter = document.getElementById("counter")
+let count = 0
+
+
+function increment(){
+    count ++
+    counter.innerHTML = count
+}
+
+
+
+
+function decrement(){
+    count --
+    counter.innerHTML = count
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
